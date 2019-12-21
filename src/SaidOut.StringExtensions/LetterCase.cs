ee082b31cb30f87c3cdd01f5c404e9479b4be9e8
Said Outgajjouft <said@agileway-itsecurity.se>
2019-12-21 02:07:05 +0100

    Xml documentation are now generated on release builds.

--- src/SaidOut.StringExtensions/LetterCase.cs
+++ src/SaidOut.StringExtensions/LetterCase.cs
@@ -1,9 +1,12 @@
 ﻿namespace SaidOut.StringExtensions
 {
 
+    /// <summary>Use to indicate the desired letter case of output.</summary>
     public enum LetterCase
     {
+        /// <summary>Indicates that letters should be in upper case.</summary>
         Upper,
+        /// <summary>Indicates that letters should be in lower case.</summary>
         Lower
     }
 }
